a3b9d13554069d981dc9dcb3e5b1b0020c107a70
Raquel Moreira <araquelmoreira@gmail.com>
2019-04-24 17:40:35 +0100

    add timer

--- src/components/TimerPlayerName.jsx
+++ src/components/TimerPlayerName.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Timer from 'react-compound-timer';
+
 
 const TimerPlayerName = () => {
+    const handleTimesUp = () => {
+        console.log('Go to next question');
+    };
+
+
     return (
         <div>
-
+                <Timer
+                    checkpoints={[
+                        {
+                            time: 3000,
+                            callback: handleTimesUp
+                        }
+                    ]}
+                >
+                    <Timer.Seconds />
+                </Timer>
         </div>
     );
 };
