01ad316619cdf623d6976ae2113b2ab68407a54a
Raquel Moreira <araquelmoreira@gmail.com>
2019-04-30 12:09:23 +0100

    add map to questions component

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -3,14 +3,14 @@ import Title from './Title';
 import TimerPlayerName from './TimerPlayerName';
 import QandA from './QandA';
 
-
 const Questions = () => {
     return (
         <div>
             <Title />
             <TimerPlayerName />
             <QandA />
-
+            <QandA />
+            <QandA />
 
         </div>
     );
