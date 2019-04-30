3e63484028fee7a3dd82c84ac31da08fc9587caf
Anelisy Nagano <anelisynagano@gmail.com>
2019-04-30 17:27:21 +0100

    map answers

--- src/components/QandA.jsx
+++ src/components/QandA.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { shuffle } from 'lodash';
 import Answer from './Answer';
 import Button from './Button';
 
 const QandA = (props) => {
     const { question } = props;
+    const shuffledAnswer = shuffle([question.correct_answer, ...question.incorrect_answers]);
     return (
         <div>
-            <div className="bg-warning p-3 mb-3 rounded">{question.question}</div>
-            <div className="border border-warning p-3 mb-3 rounded">{question.correct_answer}</div>
-            <div className="border border-warning p-3 mb-3 rounded">{question.incorrect_answers[0]}</div>
-            <div className="border border-warning p-3 mb-3 rounded">{question.incorrect_answers[1]}</div>
-            <div className="border border-warning p-3 mb-3 rounded">{question.incorrect_answers[2]}</div>
-
+            <div className="bg-warning p-3 mb-3 rounded">{(question.question)}</div>
+            {shuffledAnswer.map((answer) => {
+                return (
+                    <div key={answer} className="border border-warning p-3 mb-3 rounded">{answer}</div>
+                );
+            })}
+]
             <Answer />
             <Button />
 
