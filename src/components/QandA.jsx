412c563a40ac695b169fcd8e6931aee1013c82c8
Anelisy Nagano <anelisynagano@gmail.com>
2019-04-30 16:08:51 +0100

    call props on QandA

--- src/components/QandA.jsx
+++ src/components/QandA.jsx
@@ -3,14 +3,14 @@ import Answer from './Answer';
 import Button from './Button';
 
 const QandA = (props) => {
-    const { questions } = props;
+    const { question } = props;
     return (
         <div>
-            <div className="bg-warning p-3 mb-3 rounded">What would Diogo do?</div>
-            <div className="border border-warning p-3 mb-3 rounded">A: Tell you to Google it! </div>
-            <div className="border border-warning p-3 mb-3 rounded"> B: Give you assistance.</div>
-            <div className="border border-warning p-3 mb-3 rounded"> C: Roll his eyes...</div>
-            <div className="border border-warning p-3 mb-3 rounded"> D: *remove headphones* What?</div>
+            <div className="bg-warning p-3 mb-3 rounded">{question.question}</div>
+            <div className="border border-warning p-3 mb-3 rounded">{question.correct_answer}</div>
+            <div className="border border-warning p-3 mb-3 rounded">{question.incorrect_answers[0]}</div>
+            <div className="border border-warning p-3 mb-3 rounded">{question.incorrect_answers[1]}</div>
+            <div className="border border-warning p-3 mb-3 rounded">{question.incorrect_answers[2]}</div>
 
             <Answer />
             <Button />
