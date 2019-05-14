55cfd24ec125fc429371d0d42938852779b65f24
Anelisy Nagano <anelisynagano@gmail.com>
2019-05-14 10:33:49 +0100

    show playername on score

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Score = ({ onScore }) => {
-    console.log(onScore);
+const Score = ({ onScore, settings }) => {
     return (
 
         <div className="p-3">
-            <h1>Your Score</h1>
+            <h1>
+                <span>Congrats </span>
+                {settings.playerName}
+                !
+                Your Score is:
+            </h1>
             <div className="score">
                 <h2>
                     {onScore}
