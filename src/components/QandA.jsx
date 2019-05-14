1ac620fcd94f3766d632b71c2d54ca4c61445ea0
Raquel Moreira <araquelmoreira@gmail.com>
2019-05-14 09:35:38 +0100

    fix timer

--- src/components/QandA.jsx
+++ src/components/QandA.jsx
@@ -14,6 +14,7 @@ class QandA extends Component {
         };
     }
 
+
     componentDidMount() {
         const { question } = this.props;
         const shuffledAnswers = shuffle([question.correct_answer, ...question.incorrect_answers]);
